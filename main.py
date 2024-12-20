41354825213706ba586ba3aaffde4d7f205ef821
Miguel Alejo <miguel.sebastian.alejo@gmail.com>
2024-12-20 15:37:31 -0700

    added render function

--- main.py
+++ main.py
@@ -3,12 +3,43 @@ BOARD_DIMENSION = 3
 
 
 def new_board():
-    return [[None for _ in range(BOARD_DIMENSION)] for _ in range(BOARD_DIMENSION)]
+    board = []
+    for x in range(0, BOARD_DIMENSION):
+        column = []
+        for y in range(BOARD_DIMENSION):
+            column.append(None)
+        board.append(column)
+    return board
+
+
+def render(board):
+    rows = []
+    for x in range(BOARD_DIMENSION):
+        row = []
+        for y in range(BOARD_DIMENSION):
+            row.append(board[x][y])
+        rows.append(row)
+
+    row_num = 0
+    print("  0 1 2 ")
+    print("  -----")
+    for row in rows:
+        output_row = ""
+        for cell in row:
+            if cell is None:
+                output_row += " "
+            else:
+                output_row += cell
+        print("%d|%s|" % (row_num, " ".join(output_row)))
+        row_num += 1
+    print("  -----")
 
 
 def main():
     board = new_board()
-    print(board)
+    board[0][1] = "X"
+    board[2][0] = "O"
+    render(board)
 
 
 if __name__ == "__main__":
